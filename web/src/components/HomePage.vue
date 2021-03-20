8324e292fd7fb75dca6a31ba28c92610db603f93
Phoenix_13 <47122082+PhoenixGames-Phoenix@users.noreply.github.com>
2021-03-20 17:21:28 +0100

    Basic frontend

---
+++ web/src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Header />
+    <div class="title">
+        <h1>examplechat</h1>
+        <p class="sub">examplechat is an example chat app built using node.js, vue and flutter</p>
+    </div>
+    <button class="chatnow-button">Chat</button>
+  </div>
+</template>
+
+<script>
+import Header from './layouts/Header.vue';
+
+export default {
+  components: { Header },
+  name: "HomePage",
+}
+</script>
+
+<style scoped>
+  .title {
+    padding-top: 50px;
+  }
+  .title h1 {
+    padding-bottom: 20px;
+  }
+  .sub {
+    font-size: 20px;
+  }
+  .chatnow-button {
+    color: white;
+    background-image: linear-gradient(to right bottom, orange, red);
+    outline: none;
+    border: none;
+    font-size: 24px;
+    padding: 10px;
+    border-radius: 12px;
+  }
+</style>
---
+++ web/src/components/layouts/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+      <h1>examplechat</h1>
+      <button class="link-button" @click="redirect('https://github.com/PhoenixGames-Phoenix/examplechat')"><img src="../../assets/github.png" class="link-img"></button>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    methods: {
+        redirect(url) {
+            window.location.href = url;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .header {
+        display: inline-block;
+        text-align: center;
+        background-color: #2c3e50;
+        color: #42b983;
+        width: 100%;
+    }
+    .header h1 {
+        display: inline;
+        padding-right: 5px;
+        font-size: 32px;
+    }
+    .link-button {
+      font-size: 32px;
+      border: none;
+      outline: none;
+      border-radius: 20px;
+      background: none;
+      color: white;
+      display: inline;
+    }
+</style>
